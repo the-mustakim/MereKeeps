21cd0294c3ab379a671b14c50fb0b6ac049b51cf
Mustakim <mustakimsayyed50@gmail.com>
2023-02-07 01:46:36 +0530

    Done With this Project
    Done with MereKeeps

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
-const mongoURI='mongodb://127.0.0.1:27017/inotebook'
 
+const CONNECTION_URL = 'mongodb+srv://Mustakim:Mustakim123@clustermemories1.7mbce19.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.set('strictQuery', false);
 const connectToMongo=()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("Conncted to Mongo Successfully")
-    })
+    mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Conncted to Mongo Successfully"))
+  .catch((error) => console.log(`${error} did not connect`));
 }
 
 module.exports = connectToMongo;
